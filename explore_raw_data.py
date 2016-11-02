cd6619e672998ea02e4cf798a4332e0808028de2
jiuliu.lu@beckman.com <JIULIU.LU@BECKMAN.COM>
2016-11-02 08:53:46 -0400

    i

---
+++ explore_raw_data.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 01 13:42:14 2016
+
+@author: jllu
+"""
+
+# Explore a raw data table and generate report for each column
+#   1) Determine where a column is TimeStamp, coninuous, continuous-categorical, discrete, or something else
+#   2) Generate statis based on the column type
+#   3) Provide example, histogram, number of blank or NaN
