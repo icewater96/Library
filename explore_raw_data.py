dd288db238691dc1b2a9ea0383d1fef0fb65181f
ASUS <jianwang@gmail.com>
2016-11-03 01:16:04 -0400

    i

--- explore_raw_data.py
+++ explore_raw_data.py
@@ -9,3 +9,24 @@ Created on Tue Nov 01 13:42:14 2016
 #   1) Determine where a column is TimeStamp, coninuous, continuous-categorical, discrete, or something else
 #   2) Generate statis based on the column type
 #   3) Provide example, histogram, number of blank or NaN
+
+import pandas as pd
+import numpy  as np
+
+
+#%% 
+def explore_columns(df):
+    # By default, all columns are string now
+    column_list = list(df.columns)
+    
+    #for column in column_list:
+                
+
+
+
+#%% 
+if __name__ == '__main__':
+    # By default, all columns are string now
+    df = pd.read_csv(r'D:\GitHub\Q\L_C\Data\LoanStats3b - trimmed.csv', 
+                     header = 0, 
+                     low_memory = False)
