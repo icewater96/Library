55a5e90243dae1e7dc402627913c57ba085f57c6
ASUS <jianwang@gmail.com>
2016-11-04 01:04:33 -0400

    i

--- explore_raw_data.py
+++ explore_raw_data.py
@@ -10,6 +10,12 @@ Created on Tue Nov 01 13:42:14 2016
 #   2) Generate statis based on the column type
 #   3) Provide example, histogram, number of blank or NaN
 
+# TIP:
+#   df.types
+#   df.column.dtype
+#   Only series and dataframe data types in Pandas: 'int64', 'float64', 'object', 'datetime64'
+
+
 import pandas as pd
 import numpy  as np
 
@@ -19,10 +25,25 @@ def explore_columns(df):
     # By default, all columns are string now
     column_list = list(df.columns)
     
-    #for column in column_list:
-                
+    report_list = []
+    for column in column_list:
+        temp_data = df[column]
+        temp_dict = {}
+        temp_dict['Column Name'] = column
+        temp_dict['Data Type']   = temp_data.dtype
 
-
+        if temp_data.dtype == 'int64':
+            temp_dict['Min'] = min(temp_data)
+            temp_dict['Max'] = max(temp_data)
+            temp_dict['Mean'] = np.mean(temp_data)
+            temp_dict['Median'] = np.median(temp_data)
+            temp_dict['# of NaN'] = sum(np.isnan(df.id))
+            temp_dict['Count'] = len(temp_data)
+            temp_dict['# of Uniques'] = temp_data.nunique()
+            sorted_uniques = np.sort(temp_data.unique())
+            temp_dict['Smallest'] = str(sorted_unique.head(5))
+            
+        report_list.append(temp_dict)
 
 #%% 
 if __name__ == '__main__':
